2d9a5209497482c08c01d34f3ca5c7572f699a32
SivakumarRajagopal <sivakumar07civil@gmail.com>
2024-01-10 12:19:22 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import SpecialOffer from './SpecialOffer';
+
+export {Hero,PopularProducts,SuperQuality,Services,SpecialOffer,CustomerReviews,Subscribe,Footer}
